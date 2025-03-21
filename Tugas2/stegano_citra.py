88e8c7f0a7d8c74a1fab89deb611fb5eb53fc49e
gracialy <ly.gracies@gmail.com>
2025-03-21 07:16:24 +0700

    feat: lsb program

--- Tugas2/stegano_citra.py
+++ Tugas2/stegano_citra.py
@@ -1,24 +1,17 @@
-import stegano_api as steg
+from stegano_api import encode_lsb, decode_lsb, verify_lsb
 
-def encode_citra(cover_file: str, msg_file: str, output_file: str, key: str, is_encrypted: bool, is_sequential: bool):
-        prefix = steg.create_prefix(msg_file, is_encrypted, is_sequential)
-        msg = steg.file_to_bin(msg_file)
-        
-        if is_encrypted:
-                msg = steg.encrypt_vigenere(msg, key)
-                print(msg)
+def main():
+    cover_path = "media/hello.png"
+    msg_path = "media/sucipto.jpeg"
+    stego_path = "stego/sucipto.png"
+    extraction_path = "extraction/sucipto.jpeg"
+    stego_key = 'secretkey'
+    encryption_type = 'vigenere'
+    is_sequential = False
 
-        if is_sequential:
-                steg.encode_lsb(cover_file, prefix, msg, output_file, key, is_sequential)
-
-def decode_citra(stego_file: str, output_file: str, key: str, is_encrypted: bool, is_sequential: bool):
-        msg = steg.decode_lsb(stego_file, key)
-        
-        if is_encrypted:
-                msg = steg.decrypt_vigenere(msg, key)
-        
-        steg.bin_to_file(msg, output_file)
-        
-
-encode_citra('sucipto.jpeg', 'hello.png', 'output.png', 'asdfhalf', True, True)
-decode_citra('output.png', 'extracted.png', 'asdfhalf', True, True)
+    encode_lsb(cover_path, msg_path, stego_path, stego_key, encryption_type, is_sequential)
+    decode_lsb(stego_path, extraction_path, stego_key, encryption_type, is_sequential)
+    
+    verify_lsb(msg_path, extraction_path)
+    
+main()
