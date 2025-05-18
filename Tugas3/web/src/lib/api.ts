811689f6a02ba37240eb1b4941e1f68794ba7a87
IrfanMusthofa <im@irfanmusthofa.com>
2025-05-18 22:31:03 +0700

    prod: port change

--- Tugas3/web/src/lib/api.ts
+++ Tugas3/web/src/lib/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://103.59.160.119:8000",
   withCredentials: true, // agar session_token (cookie) ikut terkirim
 });
 
