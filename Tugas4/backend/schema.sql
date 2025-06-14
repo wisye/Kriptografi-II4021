19d3926b6cf7f0c46b6212c204c5efd25c110ff1
IrfanMusthofa <im@irfanmusthofa.com>
2025-06-14 15:41:50 +0700

    edit 4

--- Tugas4/backend/schema.sql
+++ Tugas4/backend/schema.sql
@@ -1,10 +1,11 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS academics;
-DROP TABLE IF EXISTS academic_shares;
+DROP TABLE IF EXISTS shamir_requests;
+DROP TABLE IF EXISTS shamir_shares;
 
 CREATE TABLE users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE NOT NULL UNIQUE,
+        username TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL,
         role TEXT NOT NULL CHECK (role IN ('Mahasiswa', 'Dosen Wali', 'Ketua Program Studi')),
         major TEXT NOT NULL CHECK (major IN ('IF', 'STI'))
@@ -22,12 +23,14 @@ CREATE TABLE academics (
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE academic_shares (
+CREATE TABLE shamir_shares (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         academic_id INTEGER NOT NULL REFERENCES academics (id),
         dosen_wali_id INTEGER NOT NULL REFERENCES users (id),
+        prime TEXT NOT NULL,
+        threshold INTEGER NOT NULL,
         share_x INTEGER NOT NULL,
-        share_y INTEGER NOT NULL,
+        share_y TEXT NULL,
         requested_by INTEGER NOT NULL REFERENCES users (id),
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
